bb281932262f2348fe65f2a901931bf6cec2469b
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-23 16:45:29 +0530

    Add files via upload

---
+++ prog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{ 
+ float a,b;
+ cout << "enter 2 nos"<< endl;
+ cin >> a >> b;
+ cout << "sum=" << a+b << endl; 
+ cout << "product=" << a*b << endl;
+ cout << "difference=" << a-b << endl;
+ cout << "division quotient is" << a/b << endl;
+ return 0;
+}
+
