f3d3c43b65d08ea41c391c12fc70d8a5e8ea4b50
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-23 18:50:46 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{
+ int l,b;
+ cout << " enter length and breadth of rectangle respectively" << endl;
+ cin >> l >> b;
+ cout << "the area is " << l*b;
+ return 0;
+}
