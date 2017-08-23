60f3cada9b154f49b97674d41acd8593f3ebeec2
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-23 19:03:20 +0530

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{
+ int r;
+ cout << "enter radius of circle" << endl;
+ cin >> r;
+ cout << "diameter is " << 2*r << endl;
+ cout << "circumference is " << 2*(3.14*r) << endl;
+ cout << "area is " << 3.14*(r*r) << endl;
+ return 0;
+}
+ 
