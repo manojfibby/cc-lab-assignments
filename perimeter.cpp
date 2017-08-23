2c519f9f7dea46ae940525afaee72f27ebf8a3cf
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-23 17:27:09 +0530

    Add files via upload

---
+++ perimeter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{ int l,b;
+  cout << "enter length and breadth respectively" << endl;
+  cin >> l >> b;
+  cout << "perimeter of rectangle is" << 2*(l+b);
+  return 0;
+}
