8ee2982a735a731a9d2eeb8205ca5001a656da69
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-25 19:40:40 +0530

    Add files via upload

---
+++ si.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{
+ float p,t,r;
+ cout << "enter principle time and rate respectively" << endl;
+ cin >> p >> t >> r;
+ cout << "the si calculated= " << ((p*t)*r)/100;
+ return 0;
+} 
