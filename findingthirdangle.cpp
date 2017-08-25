338f332b5b5677dd3c8c940a8dbd33734bea22ac
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-25 19:13:15 +0530

    Add files via upload

---
+++ findingthirdangle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std;
+ int main ()
+{ 
+ float a,b;
+ cout << "enter 2 angles of triangle in degrees " << endl;
+ cin >> a >> b;
+ cout << " the third angle =" << (180-(a+b));
+ return 0;
+}
