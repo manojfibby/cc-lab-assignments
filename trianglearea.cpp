24df82957b0c0889fbddce2af2e2afefafe1c04d
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-25 19:18:33 +0530

    Add files via upload

---
+++ trianglearea.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{ 
+ float b,h;
+ cout << "enter base and height of triangle" << endl;
+ cin >> b >> h;
+ cout << "area of triangle is" << ((b*h)/2);
+ return 0;
+}
