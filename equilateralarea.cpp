6d42ff360c06c0eba894d523efda2ad8f9127bee
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-25 19:24:49 +0530

    Add files via upload

---
+++ equilateralarea.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{ 
+ int a;
+ cout << "enter side of equilateral triangle" << endl;
+ cin >> a;
+ cout << " area of equilateral triangle is " << (((1.732/4)*a)*a);
+ return 0;
+}
