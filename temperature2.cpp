ed7cf6efd9be5e0d23c9a485070d9e1588f97e8e
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-25 18:56:50 +0530

    Add files via upload

---
+++ temperature2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{
+ float a;
+ cout << "enter temperature in celsius" << endl;
+ cin >> a;
+ cout << " temp in fahrenheit is" << (((a*9)/5)+32);
+ return 0;
+}
