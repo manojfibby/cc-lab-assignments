34938d075d6ee477ea0f049e4e63afd3132783dd
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-25 19:02:40 +0530

    Add files via upload

---
+++ days.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ using namespace std;
+int main ()
+{
+ float a;
+ cout << " enter number of days" << endl;
+ cin >> a;
+ cout << " numbers of weeks are " << a/7 << endl;
+ cout << " numbers of years are " << a/365 << endl;
+ return 0;
+}
