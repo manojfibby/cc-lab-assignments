e97c93dc81f08e0476e491885aa63940b8c433f1
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-25 18:36:00 +0530

    Add files via upload

---
+++ lengthconversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ using namespace std;
+int main()
+{ 
+ float a;
+ cout << "enter in centimeters"<< endl;
+ cin >> a;
+ cout << "length in meters=" << a/100 << endl;
+ cout << "length in kilometres=" << a/100000 << endl;
+ return 0;
+}
+ 
