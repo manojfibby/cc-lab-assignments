8db65906e8b3f1dea9d05fdd40e1b361663ca41c
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-30 14:47:15 +0530

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+ float a,b;
+ cout<< "enter base and exponent respectively"<< endl;
+ cin >> a >> b;
+ cout<< "the value of base to the power exponent=" << pow(a,b);
+ return 0;
+}
