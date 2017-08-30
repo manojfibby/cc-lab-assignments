be1cc500b91e0422c4bd0b6d15a47ee1a26ff8a3
manojfibby <31274629+manojfibby@users.noreply.github.com>
2017-08-30 14:53:25 +0530

    Add files via upload

---
+++ squareroot.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+ float a;
+ cout<< "enter number to calculate square root"<< endl;
+ cin >> a ;
+ cout<< "the square root for your number=" << sqrt(a);
+ return 0;
+}
